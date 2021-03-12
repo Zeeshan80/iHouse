a0c8c419f3db8093328ff3803fd4bdc09c472067
Zeeshan Aslam <zeeshan.aslam@hotmail.no>
2021-03-13 00:28:23 +0100

    test

--- main.d
+++ main.d
@@ -1,5 +1,14 @@
+<<<<<<< Updated upstream
 obj/main.o: src/main.cpp src/include/Sensors.hpp \
   src/include/LightSensor.hpp src/include/MotionSensor.hpp \
   src/include/SoundSensor.hpp src/include/TemperaturSensor.hpp \
   src/include/BathRoom.hpp src/include/Kitchen.hpp \
   src/include/LivingRoom.hpp src/include/SleepingRoom.hpp
+=======
+obj/main.o: src/main.cpp src/include/bathroom.hpp src/include/bathtub.hpp \
+  src/include/room.hpp src/include/bedroom.hpp \
+  src/include/wakeUpLight.hpp src/include/Kitchen.hpp \
+  src/include/LivingRoom.hpp src/include/heatSensor.hpp \
+  src/include/humiditySensor.hpp src/include/MotionSensor.hpp \
+  src/include/LightSensor.hpp
+>>>>>>> Stashed changes
